71b5ec4b30d084355429339bbbb46359a3746459
acordray79 <acordray79@gmail.com>
2019-06-04 16:40:12 -0400

    scaffold out property table

--- RealEstate.Data/RealEstateProperty.cs
+++ RealEstate.Data/RealEstateProperty.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace RealEstate.Data
 {
+    public enum PropertyType { Residential = 1, Commercial, Industrial, Land }
     public class RealEstateProperty
     {
+        public string ImageLink { get; set; }
+        public int SquareFootage { get; set; }
+        public string RealEstateAddress { get; set; }
+        public string RealEstateCity { get; set; }
+        public string RealEstateState { get; set; }
+        public int RealEstateZip { get; set; }
+        public PropertyType PropertyType { get; set; }
+        public bool HasBasement { get; set; }
+        public bool HasPool { get; set; }
+        public int Bedroom { get; set; }
+        public int Bathroom { get; set; }
+        public int Stories { get; set; }
     }
 }
