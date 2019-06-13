2764e0b385c9f0cf6dad638dce32f805a35378d7
umlautmatt <mahawley24@gmail.com>
2019-06-13 15:53:24 -0400

    updated buy service with mainProp properties

--- RealEstate.Data/RealEstateBuy.cs
+++ RealEstate.Data/RealEstateBuy.cs
@@ -13,22 +13,22 @@ namespace RealEstate.Data
         [Key]
         public int BuyId { get; set; }
 
-        [Required]
+
         public Guid OwnerId { get; set; }
 
-        [Required]
+
         public DateTime DateAvail { get; set; }
 
-        [Required]
+
         public double Price { get; set; }
 
-        [Required]
+
         public string Description { get; set; }
 
         [DefaultValue(false)]
-        public bool BuyFavorite { get; set; }
+        public bool? BuyFavorite { get; set; }
 
-        [Required]
+
         public int RealEstatePropertyId { get; set; }
 
         public virtual RealEstateProperty Property { get; set; }
--- RealEstate.Models/RealEstateBuys/BuyListItem.cs
+++ RealEstate.Models/RealEstateBuys/BuyListItem.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RealEstate.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,17 @@ namespace RealEstate.Models.RealEstateBuys
     {
         public int BuyId { get; set; }
         public int RealEstatePropertyId { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM} {0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime DateAvail { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+        public string RealEstatePropertyName { get; set; }
+        public PropertyType PropertyType { get; set; }
+        public string ImageLink { get; set; }
+        public string RealEstateCity { get; set; }
+        public string RealEstateState { get; set; }
+        public int Bedroom { get; set; }
     }
 }
